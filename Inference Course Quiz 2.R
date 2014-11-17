f32b1c1036593c2310ac233d11be4a62ce5d3801
RInterested <RInterested@users.noreply.github.com>
2014-11-16 23:37:36 -0500

    Update Inference Course Quiz 2.R

--- Inference Course Quiz 2.R
+++ Inference Course Quiz 2.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ##
 x <- c(0.61, 0.93, 0.83, 0.35, 0.54, 0.16, 0.91, 0.62, 0.62)
 y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
@@ -10,15 +9,12 @@ sqe <- e*e
 variance <- sum(sqe) / (length(e) - 2)
 sqrt(variance)  
 ##
-=======
->>>>>>> 35ff92f86198d71f88cdb0b06e48ad01b0376d9f
 data(mtcars)
 x <- mtcars$wt
 y <- mtcars$mpg
 fit<-lm(y ~ x)
 summary(fit)
 newdata <- data.frame(x = mean(x))
-<<<<<<< HEAD
 predict(fit, newdata, interval="confidence") 
 ##
 newdata <- data.frame(x = 3)
@@ -36,6 +32,3 @@ q <- y - (mean(y))
 p <- sum(q^2)
 r<-sum(resid(fit)^2)
 r/p
-=======
-predict(fit, newdata, interval="confidence") 
->>>>>>> 35ff92f86198d71f88cdb0b06e48ad01b0376d9f
