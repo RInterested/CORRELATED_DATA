507e4f46f89a276c5001caed52f58ad4a2e74e45
RInterested <RInterested@users.noreply.github.com>
2014-11-28 09:09:48 -0500

    Update Subsetting Hell.R

--- Subsetting Hell.R
+++ Subsetting Hell.R
@@ -31,3 +31,9 @@ Mazda RX4           21.0   6
 Mazda RX4 Wag       21.0   6
 Datsun 710          22.8   4
 Hornet 4 Drive      21.4   6
+
+
+
+> mtcars["Mazda RX4",c("mpg","cyl")]
+          mpg cyl
+Mazda RX4  21   6
