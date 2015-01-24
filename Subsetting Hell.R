1226b288e6d391f7c6527988720e33cb6d2ee249
RInterested <RInterested@users.noreply.github.com>
2015-01-24 14:42:28 -0500

    Update Subsetting Hell.R

--- Subsetting Hell.R
+++ Subsetting Hell.R
@@ -65,36 +65,28 @@ Mazda RX4  21   6
 #Select the values of a column corresponding to specific values in a separate column: 
 #We want the miles per gallon of 6 cylinder cars:
 
+USING THE $ SIGN BEFORE THE []:
+
+YES
+
 data <- mtcars$mpg[mtcars$cyl==6]
 data
 [1] 21.0 21.0 21.4 18.1 19.2 17.8 19.7
 
-head(wideCW14)
-  Diet Chick time0 time2 time4 time6 time8 time10 time12 time14 time16 time18 time20 time21 gain
-1    1    18    39    35    NA    NA    NA     NA     NA     NA     NA     NA     NA     NA   NA
-2    1    16    41    45    49    51    57     51     54     NA     NA     NA     NA     NA   NA
-3    1    15    41    49    56    64    68     68     67     68     NA     NA     NA     NA   NA
-4    1    13    41    48    53    60    65     67     71     70     71     81     91     96   55
-5    1     9    42    51    59    68    85     96     90     92     93    100    100     98   56
-6    1    20    41    47    54    58    65     73     77     89     98    107    115    117   76
+NOT USING THE $ SIGN BEFORE []:
 
-str(wideCW14)
-'data.frame':	30 obs. of  15 variables:
- $ Diet  : Factor w/ 4 levels "1","2","3","4": 1 1 1 1 1 1 1 1 1 1 ...
- $ Chick : Ord.factor w/ 50 levels "18"<"16"<"15"<..: 1 2 3 4 5 6 7 8 9 10 ...
- $ time0 : num  39 41 41 41 42 41 41 42 42 43 ...
- $ time2 : num  35 45 49 48 51 47 44 50 51 48 ...
+YES
 
-YES:
-wideCW14[wideCW14$Diet=="1","gain"]
- [1]  NA  NA  NA  55  56  76  83  NA 100 114 115 116 132 159 163 164 175 182 225 264
+mtcars[mtcars$cyl=="6","mpg"]
+[1] 21.0 21.0 21.4 18.1 19.2 17.8 19.7
 
 and...
 
 YES:
 For numerical variables, no need for " ":
-wideCW14[wideCW14$time0==41,"gain"]
- [1]  NA  NA  55  76  83 116 164 182 225 264 155 164 223
+
+mtcars[mtcars$cyl==6,"mpg"]
+[1] 21.0 21.0 21.4 18.1 19.2 17.8 19.7
 
 #DEALING WITH NA'S AFTER SUBSETTING:
 -----------------------------------
@@ -107,22 +99,23 @@ mean(wideCW14[wideCW14$time0==41,"gain"],na.rm=T)
 but...
 
 NO: 
-wideCW14["Diet"=="1","gain"]
+mtcars["cyl"==6,"mpg"]
 numeric(0)
 
 Even after attaching...
-attach(wideCW14)
-wideCW14["Diet"=="1","gain"]
+
+attach(mtcars)
+mtcars["cyl"==6,"mpg"]
 numeric(0)
 
 NO:
-wideCW14[wideCW14$Diet=="1",gain]
-Error in `[.data.frame`(wideCW14, wideCW14$Diet == "1", gain) : 
-  object 'gain' not found
+mtcars["cyl"==6,mpg]
+Error in `[.data.frame`(mtcars, "cyl" == 6, mpg) : 
+  undefined columns selected
 
 NO:
-wideCW14[wideCW14$Diet=="1",wideCW14$gain]
-Error in `[.data.frame`(wideCW14, wideCW14$Diet == "1", wideCW14$gain) : 
+mtcars[mtcars$cyl==6,mtcars$mpg]
+Error in `[.data.frame`(mtcars, mtcars$cyl == 6, mtcars$mpg) : 
   undefined columns selected
 
 
